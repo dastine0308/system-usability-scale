6b678470c4bc3a13839b497297b59385d41bfa0d
Dastine <christine.tai@fubon.com>
2023-11-10 09:46:21 +0800

    [config] change firebase account

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, doc, addDoc, getDoc, getDocs } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDgAPT7g2IXlAgYw6TR8DHPBcoyKqMNo8o',
-  authDomain: 'system-usability-scale.firebaseapp.com',
-  projectId: 'system-usability-scale',
-  storageBucket: 'system-usability-scale.appspot.com',
-  messagingSenderId: '748075587206',
-  appId: '1:748075587206:web:610397deb69e3ab889a307',
-  measurementId: 'G-34MHGKLS7J',
+  apiKey: "AIzaSyBj0gn-Qlm1MB1wBhe_EKtHS4zk4iRWkZo",
+  authDomain: "ux-sus-web.firebaseapp.com",
+  projectId: "ux-sus-web",
+  storageBucket: "ux-sus-web.appspot.com",
+  messagingSenderId: "925643843649",
+  appId: "1:925643843649:web:959186c672df893be9fec0",
+  measurementId: "G-02GY434NKH"
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
@@ -16,28 +16,30 @@ const firebaseApp = initializeApp(firebaseConfig)
 const db = getFirestore(firebaseApp)
 const resultsCollection = collection(db, 'results')
 
+// 新增測試結果
 export const createResult = async result => {
   try {
-    const docRef = await addDoc(resultsCollection, { ...result })
+    const docRef = await addDoc(resultsCollection, { ...result, date: new Date() })
     return docRef.id
   } catch (e) {
     console.error('Error adding document: ', e)
   }
 }
 
-export const getResultById = async id => {
-  try {
-    const docSnap = await getDoc(doc(db, 'results', id))
-    if (docSnap.exists()) {
-      return docSnap.data()
-    } else {
-      console.log('Document does not exist')
-    }
-  } catch (e) {
-    console.error('Error adding document: ', e)
-  }
-}
+// export const getResultById = async id => {
+//   try {
+//     const docSnap = await getDoc(doc(db, 'results', id))
+//     if (docSnap.exists()) {
+//       return docSnap.data()
+//     } else {
+//       console.log('Document does not exist')
+//     }
+//   } catch (e) {
+//     console.error('Error adding document: ', e)
+//   }
+// }
 
+// 查詢所有測試結果紀錄
 export const useLoadResults = async () => {
   const results = []
   const querySnapshot = await getDocs(resultsCollection)
