ad8586764869ece6d31bfc689219d4f8c5c42519
Dastine <christine.tai@fubon.com>
2023-11-10 09:48:04 +0800

    [3rd] add PrimeVue and moment

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column'
+import ColumnGroup from 'primevue/columngroup'
+import Row from 'primevue/row'        
+import Badge from 'primevue/badge'
+import Dropdown from 'primevue/dropdown'
 
 import App from './App.vue'
 import router from './router'
 
 import './style.css'
 import './layout.scss'
+import 'primevue/resources/themes/lara-light-teal/theme.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
+app.component('DataTable', DataTable)
+app.component('Column', Column)
+app.component('ColumnGroup', ColumnGroup)
+app.component('Row', Row)
+app.component('Badge', Badge)
+app.component('Dropdown', Dropdown)
 
 app.mount('#app')
